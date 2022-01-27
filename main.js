e2bb088a5e469147f615d75e555bdaa129d392da
Cdjrick <cdjrick@gmail.com>
2022-01-27 12:54:56 -0500

    working on project cards

---
+++ main.js
@@ -0,0 +1,79 @@
+// let section = document.getElementById('myWork')
+
+// let projects = [
+//     {
+//         src: './assets/my-work/budgeter.svg',
+//         name: 'Budgeter',
+//         description: 'Testing description',
+//         tech: ['React', 'CSS', 'Node', 'MongoDB'],
+//         buttons: ['Deployed', 'Code']
+//     },
+//     {
+//         src: './assets/my-work/budgeter.svg',
+//         name: 'Budgeter',
+//         description: 'Testing description',
+//         tech: ['React', 'CSS', 'Node', 'MongoDB'],
+//         buttons: ['Deployed', 'Code']
+//     },
+//     {
+//         src: './assets/my-work/budgeter.svg',
+//         name: 'Budgeter',
+//         description: 'Testing description',
+//         tech: ['React', 'CSS', 'Node', 'MongoDB'],
+//         buttons: ['Deployed', 'Code']
+//     },
+//     {
+//         src: './assets/my-work/budgeter.svg',
+//         name: 'Budgeter',
+//         description: 'Testing description',
+//         tech: ['React', 'CSS', 'Node', 'MongoDB'],
+//         buttons: ['Deployed', 'Code']
+//     },
+//     {
+//         src: './assets/my-work/budgeter.svg',
+//         name: 'Budgeter',
+//         description: 'Testing description',
+//         tech: ['React', 'CSS', 'Node', 'MongoDB'],
+//         buttons: ['Deployed', 'Code']
+//     },
+//     {
+//         src: './assets/my-work/budgeter.svg',
+//         name: 'Budgeter',
+//         description: 'Testing description',
+//         tech: ['React', 'CSS', 'Node', 'MongoDB'],
+//         buttons: ['Deployed', 'Code']
+//     },
+//     {
+//         src: './assets/my-work/budgeter.svg',
+//         name: 'Budgeter',
+//         description: 'Testing description',
+//         tech: ['React', 'CSS', 'Node', 'MongoDB'],
+//         buttons: ['Deployed', 'Code']
+//     }
+// ]
+
+// projects.forEach(project => {
+//     let div = document.createElement('div')
+//     let p = document.createElement('p')
+//     project.forEach(tech => {
+//         p.innerHTML =  tech.tech
+//     })
+//     div.innerHTML = `
+//         <div class="card col-12 col-lg-4 mx-auto mt-4" style="width: 25rem;">
+//             <img src="./assets/my-work/budgeter.svg" class="card-img-top" alt="...">
+//             <div class="card-body">
+//                 <h5 class="card-title">${project.name}</h5>
+//                 <p class="card-text">${project.description}</p>
+//                 <div class='row my-3 mx-auto'>
+//                     <p class="card-text col-auto mx-auto mt-2">${project.tech}</p>
+//                 </div>
+//                 <div class='row'>
+//                     <a href="#" class="btn btn-primary col-auto mx-auto">${project.buttons}</a>
+//                 </div>
+//             </div>
+//         </div>
+//     `
+
+//     section.appendChild(div)
+// })
+
